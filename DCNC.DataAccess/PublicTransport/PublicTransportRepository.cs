e298f35100f256a8d942c2671141e5122d378e7b
Jan Miętki <jan.mietki@gmail.com>
2019-03-12 15:55:40 +0100

    Repository update - rework of HttpClient usage

--- DCNC.DataAccess/PublicTransport/PublicTransportRepository.cs
+++ DCNC.DataAccess/PublicTransport/PublicTransportRepository.cs
@@ -5,17 +5,11 @@ namespace DCNC.DataAccess.PublicTransport
 {
     public class PublicTransportRepository
     {
+        private static readonly HttpClient Client = new HttpClient();
+
         public static async Task<string> DownloadData(string url)
         {
-            var data = "";
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(url);
-                var json = await response.Content.ReadAsStringAsync();
-                data = json;
-            }
-
-            return data;
+            return await Client.GetStringAsync(url);
         }
     }
 }
