2ab695fadea73572dcb5de565d675b724d7821b9
Jan Miętki <jan.mietki@gmail.com>
2019-04-22 12:52:26 +0200

    Removed most of unused properties in BusStopDataModel

--- DoCeluNaCzasWebApi/Models/PublicTransport/General/BusStopDataModel.cs
+++ DoCeluNaCzasWebApi/Models/PublicTransport/General/BusStopDataModel.cs
@@ -12,19 +12,9 @@ namespace DoCeluNaCzasWebApi.Models.PublicTransport.General
     public class StopModel
     {
         public int StopId { get; set; }
-        public string StopCode { get; set; }
-        public string StopName { get; set; }
-        public string StopShortName { get; set; }
         public string StopDesc { get; set; }
-        public string SubName { get; set; }
-        public DateTime Date { get; set; }
         public double StopLat { get; set; }
         public double StopLon { get; set; }
-        public int? ZoneId { get; set; }
-        public string ZoneName { get; set; }
-        public bool? VirtualBusStop { get; set; }
-        public bool? NonPassenger { get; set; }
-        public bool? Depot { get; set; }
         public bool? TicketZoneBorder { get; set; }
         public bool? OnDemand { get; set; }
         public DateTime ActivationDate { get; set; }
